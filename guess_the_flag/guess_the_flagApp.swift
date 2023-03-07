71d1eec1f6f55153c63a0af9889636d0e38b67b2
David Olagookun <erijesudo@gmail.com>
2023-03-07 08:31:15 +0100

    Initial Commit

---
+++ guess_the_flag/guess_the_flagApp.swift
@@ -0,0 +1,17 @@
+//
+//  guess_the_flagApp.swift
+//  guess_the_flag
+//
+//  Created by David OH on 07/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct guess_the_flagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
